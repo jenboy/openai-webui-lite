d00805d068c8144d33fa1e45ef4748e053498086
icheer <icheer@live.com>
2026-01-15 22:09:19 +0800

    fix: ui

--- worker.js
+++ worker.js
@@ -2059,7 +2059,7 @@ function getHtmlContent(modelIds, tavilyKeys, title) {
 
       .settings-section {
         text-align: right;
-        margin-top: 5px;
+        margin-top: 4px;
         margin-bottom: 15px;
       }
 
